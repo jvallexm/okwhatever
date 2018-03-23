49e1a7b5ab3a93b473c08a6686d44493dba534c8
Jenny Valle <jennyvalle@Jennys-MacBook-Air.local>
2018-03-23 13:45:31 -0400

    Trying to get express session to work

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,6 +4,7 @@ module.exports = function(passport,app){
  
     app.get('/auth/facebook/callback?',(req,res)=>{
         req.session.logged_in = true;
+        console.log(req.body);
         res.send("fff");
     });
 }
