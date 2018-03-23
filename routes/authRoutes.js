21a784d9d9de066058b062add7085501833448b5
Jenny Valle <jennyvalle@Jennys-MacBook-Air.local>
2018-03-23 14:06:48 -0400

    More express still

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -13,12 +13,13 @@ module.exports = function(app){
       }
     ));
 
-    app.get('/auth/facebook',passport.authenticate('facebook'));
- 
+    app.get('/auth/facebook',
+        passport.authenticate('facebook'));
+
     app.get('/auth/facebook/callback',
-    -  passport.authenticate('facebook', { failureRedirect: '/login' }),
-    -  function(req, res) {
-    -    // Successful authentication, redirect home. 
-    -    res.send("ding");
+        passport.authenticate('facebook', { failureRedirect: '/login' }),
+        function(req, res) {
+            // Successful authentication, redirect home.
+            res.redirect('/');
     });
 }
