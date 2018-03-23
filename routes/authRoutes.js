a334677c627bda1043e2674129681a9118c00004
Jenny Valle <jennyvalle@Jennys-MacBook-Air.local>
2018-03-23 13:53:03 -0400

    More express still

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -17,6 +17,6 @@ module.exports = function(app){
     app.get('/auth/facebook',passport.authenticate('facebook'));
  
     app.get('/auth/facebook/callback?',(req,res)=>{
-        res.send("fff");
+        console.log("well");
     });
 }
