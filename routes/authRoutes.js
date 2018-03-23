2de1d3442e734575d5d6b2a25195c3a5b1b2e4e9
Jenny Valle <jennyvalle@Jennys-MacBook-Air.local>
2018-03-23 14:09:10 -0400

    More express still

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,7 +6,7 @@ module.exports = function(app){
     passport.use(new FacebookStrategy({
         clientID: process.env.FACEBOOK_APP_ID,
         clientSecret: process.env.FACEBOOK_APP_SECRET,
-        callbackURL: "/auth/facebook/callback?"
+        callbackURL: "/auth/facebook/callback"
       },
       function(accessToken, refreshToken, profile, cb) {
         console.log(JSON.stringify(profile));
@@ -15,7 +15,7 @@ module.exports = function(app){
 
     app.get('/auth/facebook', passport.authenticate('facebook'));
 
-    app.get('/auth/facebook/callback?',
+    app.get('/auth/facebook/callback',
         passport.authenticate('facebook', { failureRedirect: '/login' }),
         function(req, res) {
             // Successful authentication, redirect home.
