737bfac9301728209dc202dbc6778ee9ddf7c3f2
Jenny Valle <jennyvalle@Jennys-MacBook-Air.local>
2018-03-22 21:44:33 -0400

    sends ding when done

--- server.js
+++ server.js
@@ -21,9 +21,8 @@ passport.use(new FacebookStrategy({
     clientID: process.env.FACEBOOK_APP_ID,
     clientSecret: process.env.FACEBOOK_APP_SECRET,
     callbackURL: "/auth/facebook/callback"
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    return "hot poppers";
+  },()=>{
+      console.log("Hot poppers")
   }
 ));
 
