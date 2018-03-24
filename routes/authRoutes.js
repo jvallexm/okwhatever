1c0564872846a831c86df4407ed1b27377bcf705
Jenny Valle <jennyvalle@Jennys-MacBook-Air.local>
2018-03-24 13:08:08 -0400

    age range

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -23,7 +23,7 @@ module.exports = function(app,jwt){
         clientID: process.env.FACEBOOK_APP_ID,
         clientSecret: process.env.FACEBOOK_APP_SECRET,
         callbackURL: "/auth/facebook/callback", 
-        profileFields: ['email','birthday','first_name','location','picture.type(large)','likes','music','movies','television']
+        profileFields: ['email','birthday','first_name','age_range','location','picture.type(large)','likes','music','movies','television']
       },
       function(accessToken, refreshToken, profile, cb) {
         console.log(JSON.stringify(profile)); // Logs profile data
