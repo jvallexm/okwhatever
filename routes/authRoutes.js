25b13dff8ff8eff5194a6a353111c224c7e753c1
Jenny Valle <jennyvalle@Jennys-MacBook-Air.local>
2018-03-24 11:10:45 -0400

    Fixing redirects

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -23,7 +23,7 @@ module.exports = function(app,jwt){
         clientID: process.env.FACEBOOK_APP_ID,
         clientSecret: process.env.FACEBOOK_APP_SECRET,
         callbackURL: "/auth/facebook/callback", 
-        profileFields: ['email','birthday','first_name','location','photos']
+        profileFields: ['email','birthday','first_name','location','photos','likes']
       },
       function(accessToken, refreshToken, profile, cb) {
         console.log(JSON.stringify(profile)); // Logs profile data
