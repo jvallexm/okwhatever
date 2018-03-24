315d5ca481a0fbac3c56e8d1475876411ef87f88
Jenny Valle <jennyvalle@Jennys-MacBook-Air.local>
2018-03-24 10:44:55 -0400

    Maybe read cookie client side?

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -2,3 +2,13 @@ $("#login").on("click",function(){
     console.log("login..");
     window.open(`/auth/facebook`);
 });
+
+$(document).ready(()=>{
+    function checkCookie() {
+        var username = getCookie("auth");
+        if (username != "") {
+            alert("Welcome again " + username);
+        }
+    }
+    checkCookie();
+});
