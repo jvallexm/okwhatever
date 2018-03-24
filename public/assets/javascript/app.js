4bd54183dbbaf11aaf9799b4bcbe7c48e3ad74c7
Jenny Valle <jennyvalle@Jennys-MacBook-Air.local>
2018-03-24 10:45:41 -0400

    Maybe read cookie client side?

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -4,6 +4,21 @@ $("#login").on("click",function(){
 });
 
 $(document).ready(()=>{
+    function getCookie(cname) {
+        var name = cname + "=";
+        var decodedCookie = decodeURIComponent(document.cookie);
+        var ca = decodedCookie.split(';');
+        for(var i = 0; i <ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return "";
+    }
     function checkCookie() {
         var username = getCookie("auth");
         if (username != "") {
