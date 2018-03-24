57bdab63845952ce5a71eee45f57549959dede3e
Jenny Valle <jennyvalle@Jennys-MacBook-Air.local>
2018-03-24 13:25:18 -0400

    Done with fb permissions for now

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -23,7 +23,7 @@ module.exports = function(app,jwt){
         clientID: process.env.FACEBOOK_APP_ID,
         clientSecret: process.env.FACEBOOK_APP_SECRET,
         callbackURL: "/auth/facebook/callback", 
-        profileFields: ['email','birthday','first_name','friends','age_range','about','location','picture.type(large)','likes','music','movies','television']
+        profileFields: ['email','birthday','first_name','age_range','about','location','picture.type(large)','likes','music','movies','television']
       },
       function(accessToken, refreshToken, profile, cb) {
         console.log(JSON.stringify(profile)); // Logs profile data
@@ -46,7 +46,7 @@ module.exports = function(app,jwt){
     app.use(passport.initialize());
     app.use(passport.session());
 
-    app.get('/auth/facebook', passport.authenticate('facebook',{authType: 'rerequest', scope: ['user_likes','user_friends','email','public_profile','user_birthday','user_location','user_photos','user_actions.music','user_actions.movies'] }));
+    app.get('/auth/facebook', passport.authenticate('facebook',{authType: 'rerequest', scope: ['user_likes','public_profile','user_birthday','user_location','user_photos','user_actions.music','user_actions.movies'] }));
 
     app.get('/auth/facebook/callback',
         passport.authenticate('facebook', { failureRedirect: '/login' }),
