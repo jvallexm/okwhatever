ec5850bd1d33ef42757e684cd92ae6b1b4d22099
Jenny Valle <jennyvalle@Jennys-MacBook-Air.local>
2018-03-25 08:06:16 -0400

    Added route skelleton, more robust user info

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,43 @@
+module.exports = function(app){
+
+    // Route to edit your profile
+    app.get(`/edit`,(req,res)=>{
+
+
+
+    });
+
+    // Route to view a profile
+
+    app.get(`/users/:id`,(req,res)=>{
+
+
+
+    });
+
+    // Route to check inbox
+
+    app.get(`/inbox`,(req,res)=>{
+
+
+
+    });
+
+    // Route to screen of matches
+
+    app.get(`/matches`,(req,res)=>{
+
+
+
+    });
+
+    // Route to logout
+
+
+    app.get(`/logout`,(req,res)=>{
+
+
+
+    });
+
+}
