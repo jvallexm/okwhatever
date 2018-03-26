58205e4bfe2536e032cd76955eff598ff99f9d63
Jenny Valle <jennyvalle@Jennys-MacBook-Air.local>
2018-03-26 15:01:46 -0400

    Trying to render matches

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -36,7 +36,10 @@ module.exports = function(app){
 
     app.get(`/matches`,(req,res)=>{
 
-
+        res.render("index",{
+            sideuser: test,
+            matches: []
+        });
 
     });
 
