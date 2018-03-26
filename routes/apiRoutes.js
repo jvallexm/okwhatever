d00c89a140e2f1806a2028100f6a0358477d515b
Amy <amysuelumpkin@gmail.com>
2018-03-26 19:30:42 -0400

    message

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,15 +1,35 @@
+const db = require("../models/index");
+
 module.exports = function(app){
 
-    app.get('/api/profile/:id',(req,res)=>{
-
+    app.post("/api/new", (req,res)=>{
+        console.log("creating a new user");
+        db.user.create({
+            id: "placeholder",
+            image: "placeholder",
+            name: "placeholder"
+        }).then(user => res.redirect("/"))
     });
-
+    //get single user
+    app.get('/api/profile/:id',(req,res)=>{
+        if (req.params.id){
+            db.user.findAll({ where: { id: req.params.id }})
+            .then(results => res.json(results));
+        }
+    });
+    //updating a user
     app.post('/api/profile/:id',(req,res)=>{
-        
+        console.log("updating req.body");
+        db.user.update(
+            req.body,
+            {where: {id: req.body.id}}
+        )
+        .then(result => res.json(result))
+        .catch(err => res.json(err));
     })
-
+    //get all users
     app.get('/api/users',(req,res)=>{
-
+        db.user.findAll({}).then(results => res.json(results));
     });
 
     app.get('/api/messages/:user',(req,res)=>{
