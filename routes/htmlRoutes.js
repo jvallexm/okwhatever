b8cd162dd342aa6c4afb0eb1def2fd8cde6586f9
Jenny Valle <jennyvalle@Jennys-MacBook-Air.local>
2018-03-26 15:14:58 -0400

    Renders matches kind of

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -36,10 +36,7 @@ module.exports = function(app){
 
     app.get(`/matches`,(req,res)=>{
 
-        res.render("index",{
-            sideuser: "test",
-            matches: []
-        });
+        res.render("index");
 
     });
 
