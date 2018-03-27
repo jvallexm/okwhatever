ea7812ad792112ced3ae7d2f386d504aa2a093cc
Jenny Valle <jennyvalle@Jennys-MacBook-Air.local>
2018-03-26 22:20:30 -0400

    Added some comments

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -20,37 +20,44 @@ module.exports = function(app){
     app.get('/api/profile/:id',(req,res)=>{
         if (req.params.id){
             db.user.findAll({ where: { id: req.params.id }})
-            .then(results => res.json(results));
+                   .then(results => res.json(results));
         }
     });
 
     //updating a user
     app.post('/api/profile/update',(req,res)=>{
+
         console.log("updating user " + req.user_data.id);
-        console.log(req.body);
-        let id = req.user_data.id;
-        let update = req.body;
+        // console.log(req.body);
+
+        let id = req.user_data.id; // Id of the user being updated
+        let update = req.body;     // Object being sent to the API
+
         if(update.birthday && update.bio && update.gender && update.faves && update.city && update.state)
-            update.complete = true;
-        db.user.update(
+            update.complete = true; // If the required fields are filled out it marks your profile as complete
+
+        db.user.update(  // Updates the db
             update,
             {where: {id: id}}
         )
-        .then(result => res.json(result))
-        .catch(err => res.json(err));
+        .then(result => res.json(true)) // Returns true once it's complete
+        .catch(err => res.json(err));   // Otherwise returns the error
+
     });
 
     //get all users
     app.get('/api/users',(req,res)=>{
-        db.user.findAll({}).then(results => res.json(results));
+        db.user.findAll({})
+               .then(results => res.json(results));
     });
 
     //get messages sent FROM an id and TO an id:
-    app.get('/api/messages/:user',(req,res)=>{
-        if (req.parms.fromId){
-            db.message.findAll({ where: { fromId: req.params.fromId }/{ toId: req.params.toId }})
-            .then (results => res.json(results));
-        }
+    app.get('/api/messages/',(req,res)=>{
+
+        let id = req.user_data.id;
+
+        db.message.findAll({ where: { fromId: id }/{ toId: id }})
+                  .then(results => res.json(results));
     });
     
     app.post('/api/messages/:user',(req,res)=>{
