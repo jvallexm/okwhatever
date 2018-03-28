7388b82db0202eb51b1e165afee271ed04c70ff4
Emanuel Siu <e@emanuelsiu.com>
2018-03-28 19:58:23 -0400

    cheapo validation done

--- public/assets/javascript/validation.js
+++ public/assets/javascript/validation.js
@@ -10,47 +10,83 @@ $(document).ready(function () {
     let favThing3 = Boolean;
     let bio = Boolean;
     let interested = Boolean;
-    
 
-    if ($('.datepicker').val() === "") {
-        date = false;
-    };
-    if ($('#gender').val() === null) {
-        gender = false;
-    };
-    if ($('#sexuality').val() === null) {
-       sexuality = false;
-    };
-    if ($('#favorite1').val() === "") {
-        favThing1 = false;
-    };
-    if ($('#favorite2').val() === "") {
-        favThing2 = false;
-    };
-    if ($('#favorite3').val() === "") {
-        favThing3 = false;
-    };
-    if ($('#bio').val() === "") {
-        bio = false;
-    };
-    if ($('#interestedIn').val() === "") {
-        interested = false;
-    };
 
-    // info object to send to post user
-    var validationData = {
-        date: "empty date",
-        gender: "no gender",
-        sexuality: "no sexuality"
-      };
+  
 
     $(".btn").on("click", function(event) {
         // Make sure to preventDefault on a submit event.
         event.preventDefault();
-    
+        
+        let isValidated = false;
+
+        $("#validation").text("")
+
+        if ($('.datepicker').val() === "") {
+            date = false;
+        } else {date = true;};
+
+        if ($('#gender').val() === null) {
+            gender = false;
+        }else {gender = true;};
+
+        if ($('#sexuality').val() === null) {
+           sexuality = false;
+        }else {sexuality = true;};
+
+        if ($('#favorite1').val() === "") {
+            favThing1 = false;
+        }else {favThing1 = true;};
+
+        if ($('#favorite2').val() === "") {
+            favThing2 = false;
+        }else {favThing2 = true;};
+
+        if ($('#favorite3').val() === "") {
+            favThing3 = false;
+        }else {favThing3 = true;};
+
+        if ($('#bio').val() === "") {
+            bio = false;
+        }else {bio = true;};
+        
+        if ($('#interestedIn').val() === "") {
+            interested = false;
+        }else {interested = true;};
+
         if (date===false){
-            alert("fill out the date");
-        }
+            $("#validation").append(" please fill out a date. ");
+        };
+        if (gender===false){
+            $("#validation").append(" please fill out a gender. ");
+        };
+        if (sexuality===false){
+            $("#validation").append(" please fill out a sexuality. ");
+        };
+        if (favThing1===false){
+            $("#validation").append(" please fill out favorite thing 1. ");
+        };
+        if (favThing2===false){
+            $("#validation").append(" please fill out favorite thing 2. ");
+        };
+        if (favThing3===false){
+            $("#validation").append(" please fill out favorite thing 3. ");
+        };
+        if (bio===false){
+            $("#validation").append(" please fill out a bio. ");
+        };
+        if (interested===false){
+            $("#validation").append(" please fill out some of your interests. ");
+        };
+
+        if ($("#validation").val().trim() === ""){
+            isValidated = true;
+        };
+
+        console.log(isValidated);
+
     });
 
 });
+
+
