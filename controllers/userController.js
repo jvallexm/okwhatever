1479c49f3e20bd29d4874cd6306a56724c3e33d4
Jenny Valle <jennyvalle@Jennys-MacBook-Air.local>
2018-03-28 18:01:10 -0400

    Added userController

---
+++ controllers/userController.js
@@ -0,0 +1,16 @@
+const db = require('../models/index');
+
+module.exports = {
+
+    findOne: function(req,cb){
+
+        db.user.findAll({where: {id: req.user.user_id}})
+               .then(arr=>{
+
+                    cb(arr[0]);
+
+               })
+
+    }
+
+}
