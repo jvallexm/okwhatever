6ec3fe538d2fe47b4c4bef5390795849c6ba28a8
Jenny Valle <jennyvalle@Jennys-MacBook-Air.local>
2018-03-29 17:17:31 -0400

    inbox and outbox

---
+++ controllers/messageController.js
@@ -0,0 +1,20 @@
+const db = require('../models/index');
+
+module.exports = {
+
+    inbox: function(user,cb){
+
+        db.message.findAll({where: {toId: user},
+                            include: [db.user]})
+                  .then(inbox => cb(inbox));
+
+    },
+
+    outbox: function(user,cb){
+
+        db.message.findAll({ where: { fromId: user }, include: [{model: db.user, as: "to"}]})
+        .then(inbox => cb(inbox));
+
+    }
+
+}
