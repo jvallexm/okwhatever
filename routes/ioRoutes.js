aee86e7f4260a1eafbb7a0f14bf1dcb9b63ced0b
Jenny Valle <jennyvalle@Jennys-MacBook-Air.local>
2018-03-29 10:29:44 -0400

    Disconnect

--- routes/ioRoutes.js
+++ routes/ioRoutes.js
@@ -2,22 +2,30 @@ const jwt          = require('jsonwebtoken');
 
 module.exports = function(io){
 
-    const users = [];
+    function parseClientCookie(client,cb){
 
-    io.on("connection",(client)=>{
-        console.log("Someone done connected");
-        users.push(client);
-        console.log("total users " + users.length);
         let cookie = client.handshake.headers.cookie;
         let authSplit = cookie.split("auth=")[1];
         let token = authSplit.split(";")[0];
         jwt.verify(token,process.env.COOKIE_SECRET,(err,data)=>{
     
             if(data)
-                console.log(data);
+                return data;
     
         });
         
+
+    }
+
+    const users = [];
+
+    io.on("connection",(client)=>{
+        console.log("Someone done connected");
+        client.user_data = parseClientCookie(client);
+        console.log(client.user_data);
+        users.push(client);
+        console.log("total users " + users.length);
+
         client.on("disconnect",(client)=>{
             console.log("someone done disconnected");
             users.splice(users.indexOf(client),1);
