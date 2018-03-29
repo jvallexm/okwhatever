53a2d25374a3659a8fa560e86689248e3c1d364b
Jenny Valle <jennyvalle@Jennys-MacBook-Air.local>
2018-03-29 09:13:24 -0400

    Sends to not from

--- models/message.js
+++ models/message.js
@@ -23,7 +23,7 @@ module.exports = function(sequelize, DataTypes){
         }});
         message.belongsTo(models.user, 
         {   
-            as: "from",
+            as: "to",
             foreignKey: {
                 allowNull: false
         }});
