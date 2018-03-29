b50bc162c8eff95ab3d86c3d33c42852cddb6451
Jenny Valle <jennyvalle@Jennys-MacBook-Air.local>
2018-03-29 07:49:24 -0400

    Trying to mess with sequelize

--- models/message.js
+++ models/message.js
@@ -18,7 +18,7 @@ module.exports = function(sequelize, DataTypes){
 
         message.belongsTo(models.user,{
             foreignKey: "fromId",
-            targetKey: "fromId"
+            targetKey: "id"
         });
 
     }
