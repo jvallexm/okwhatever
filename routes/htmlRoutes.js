d77c46effe84d16d2b92629cb7fa0123d32bba91
Jenny Valle <jennyvalle@Jennys-MacBook-Air.local>
2018-03-29 07:22:17 -0400

    Working on inbox route

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -69,7 +69,7 @@ module.exports = function(app){
         
         if(req.user_data){
             let user = req.user_data.id;
-            db.message.findAll({ where: { fromId: user }/{ toId: user }})
+            db.message.findAll({ where: { fromId: user }})
                     .then(results =>{
 
                         let send = {
