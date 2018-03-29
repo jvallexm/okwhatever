c20f27603986a2122a87f38409df99bed09beaf6
Jenny Valle <jennyvalle@Jennys-MacBook-Air.local>
2018-03-29 19:23:19 -0400

    Ugh

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -30,7 +30,7 @@ module.exports = function(app,path){
         clientID: process.env.FACEBOOK_APP_ID,
         clientSecret: process.env.FACEBOOK_APP_SECRET,
         callbackURL: "/auth/facebook/callback", 
-        profileFields: ['email','birthday','first_name']
+        profileFields: ['email','birthday','first_name','picture']
 
       },
       function(accessToken, refreshToken, profile, cb) {
