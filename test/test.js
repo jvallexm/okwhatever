2f2260c33671f40cf41bd10d1da9a5dcc9c2765a
Amy <amysuelumpkin@gmail.com>
2018-03-30 13:13:01 -0400

    testing

---
+++ test/test.js
@@ -0,0 +1,20 @@
+const chai = require("chai");
+const expect = require("chai").expect;
+const app = require("../server");
+chai.use(require("chai-http"));
+
+const userController = require("../controllers/userController");
+
+//testing if userController gets all users (findAll function)
+describe("findAll", function(){
+    it("should display all users", function(){
+        return chai.request(app).get("/api/users").then(function(res){
+            console.log(res.body);
+            expect(res.body).to.be.an("array");
+        })
+
+});
+
+    
+
+});
