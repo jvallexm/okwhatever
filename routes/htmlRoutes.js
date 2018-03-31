a8cd67fb8adc6d1ca911aac1f6b73f8496dbae92
Jenny Valle <jennyvalle@Jennys-MacBook-Air.local>
2018-03-31 07:25:32 -0400

    Rendering you for test

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -210,6 +210,7 @@ module.exports = function(app){
 
                     if(r[i].id === itYou){
                         you = r[i];
+                        matches.push(you)
                         //matches.push(r[i]);
                     } else if(r[i].complete) {
                         let faves = r[i].faves.split(";;;").join(" - ");
