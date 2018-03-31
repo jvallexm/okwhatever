377ffcbe21f847bab0f8f2628fe8905e686384c4
Jenny Valle <jennyvalle@Jennys-MacBook-Air.local>
2018-03-30 20:56:58 -0400

    Birthday stuff

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -69,7 +69,7 @@ $(document).ready(() => {
         e.preventDefault();
 
         let newUser = {
-            birthday:      new Date(newUser.birthday).getTime()/1000,
+            birthday:      new Date($(`#birthday`).val()).getTime()/1000,
             bio:           $("#bio").val().trim(),
             gender:        $("#gender").val().trim(),
             interested_in: $("#sexuality").val().trim(),
